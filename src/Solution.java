566da43b39f61ae5812f67f2bc02dfb0f877e9e2
rohanthomas1202 <rst170000@utdallas.edu>
2020-02-27 12:18:19 -0600

    added parent node in node class

--- src/Solution.java
+++ src/Solution.java
@@ -6,10 +6,9 @@ public class Solution {
 
         List<Integer> index_list = new LinkedList<>();
 
-        index_list.add(7);
-        index_list.add(3);
-        index_list.add(5);
         index_list.add(1);
+        index_list.add(4);
+        index_list.add(7);
 
 
         /*
@@ -37,14 +36,10 @@ public class Solution {
     }
 
 
-
-
     public static List<Integer> solution(int h, List<Integer> q) {
         int list_size = q.size();
 
-        List<Integer> flux_converter_label= new LinkedList<>();
-
-
+        List<Integer> flux_converter_label = new LinkedList<>();
 
 
         Node node = new Node(0);
@@ -55,13 +50,12 @@ public class Solution {
         flux_chain_design_tree.createTree();
 
         flux_chain_design_tree.post_order_labeling(flux_chain_design_tree.root);
-        flux_chain_design_tree.post_order_indexing(flux_chain_design_tree.root);
+        //flux_chain_design_tree.post_order_indexing(flux_chain_design_tree.root);
 
 
         for (Integer integer : q) {
-            flux_converter_label.add(flux_chain_design_tree.val_from_index(integer));
+            flux_converter_label.add(flux_chain_design_tree.find_parent(integer));
         }
-
 
 
         return flux_converter_label;
@@ -87,6 +81,8 @@ public class Solution {
             Queue<Node> flux_queue = new LinkedList<>();
             flux_queue.add(curr_node);
 
+            Node curr_left, curr_right;
+
 
             for (int i = 1, j = 1; i < (Math.pow(2, height) - 1); i += 2, j++) {
                 curr_node = flux_queue.remove();
@@ -94,6 +90,13 @@ public class Solution {
                 flux_queue.add(curr_node.left);
                 curr_node.right = new Node(-j);
                 flux_queue.add(curr_node.right);
+
+                curr_left = curr_node.left;
+                curr_right = curr_node.right;
+
+
+                curr_left.parent = curr_node;
+                curr_right.parent = curr_node;
 
             }
 
@@ -130,8 +133,10 @@ public class Solution {
 
         /*
         Function to index flux chains in the desired order (Parent --> Left_Child --> Right_Child)
+
          */
-       /* public void pre_order_indexing(Node node) {
+/*
+        public void pre_order_indexing(Node node) {
             if (node == null)
                 return;
 
@@ -145,12 +150,14 @@ public class Solution {
 
             // go down right subtree recursively
             pre_order_indexing(node.right);
-        }*/
+        }
 
-       public void post_order_indexing(Node node) {
+*/
+/*
+
+        public void post_order_indexing(Node node) {
             if (node == null)
                 return;
-
 
 
             // go down left subtree recursively
@@ -159,14 +166,15 @@ public class Solution {
 
             // go down right subtree recursively
             post_order_indexing(node.right);
-           // update index values
-           node.index = index;
-           index++;
+            // update index values
+            node.index = index;
+            index++;
 
         }
+*/
+/*
 
-
-       /*public void in_order_indexing(Node node) {
+       public void in_order_indexing(Node node) {
             if (node == null)
                 return;
 
@@ -183,31 +191,37 @@ public class Solution {
            in_order_indexing(node.right);
 
 
-        }*/
-
-
-
-
-
-        // find the flux_value given the index of node
-        public int val_from_index(int index) {
-            Node curr_node = root;
-            Queue<Node> val = new LinkedList<>();
-
-            val.add(curr_node);
-            while (val.peek() != null) {
-                curr_node = val.remove();
-
-                if (curr_node.index == index) {
-                    return curr_node.flux_converter_ID;
-                }
-                val.add(curr_node.left);
-                val.add(curr_node.right);
-            }
-            return -1;
         }
 
+*/
 
+        // find the flux_value given the index of node
+        public int find_parent(int check_val) {
+            Node curr_node = root;
+            Queue<Node> queue = new LinkedList<>();
+
+            queue.add(curr_node);
+            while (queue.peek() != null) {
+                curr_node = queue.remove();
+
+                if (check_val == curr_node.flux_converter_ID) {
+                    if (curr_node != root) {
+                        return (curr_node.parent.flux_converter_ID);
+                    }
+                    else{
+                        return -1;
+                    }
+                }
+
+               /* if (curr_node.index == index) {
+                    return curr_node.flux_converter_ID;
+                }*/
+                queue.add(curr_node.left);
+                queue.add(curr_node.right);
+            }
+            return -1;
+
+        }
 
 
     }
@@ -215,8 +229,10 @@ public class Solution {
     public static class Node {
 
         int flux_converter_ID;
-        int index;
+        int parent_val;
 
+
+        Node parent;
         Node left;
         Node right;
 
